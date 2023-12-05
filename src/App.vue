637b0b4a0ce6b847f218ba6c7682fdc655a07a22
Ian Irving <ian@falsepositives.com>
2023-12-05 17:20:25 -0500

    Add TheNavigation component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
-<script>
-
-</script>
 
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
-
   <div class="container">
+    <TheNavigation />
     <router-view />
   </div>
 </template>
+
+<script>
+import TheNavigation from "./components/TheNavigation.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheNavigation
+  }
+};
+
+</script>
+
 
 <style></style>
