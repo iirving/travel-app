cd1df11fa571f51909a01315bee1b46b31b1ea6d
Ian Irving <ian@falsepositives.com>
2023-12-14 17:27:28 -0500

    Add NotFound component to handle 404 errors

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,11 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Not Found</h1>
+    <p>
+      Opps, we couldn't find what you were looking for.
+      Try going to the <router-link to="/">home page</router-link>.
+    </p>
+  </div>
+</template>
