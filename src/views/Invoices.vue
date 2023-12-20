817bc99bfdb726ad17680a8ca908cd1ce352b2a1
Ian Irving <ian@falsepositives.com>
2023-12-20 18:37:30 -0500

    Add route leave confirmation dialog to Invoices view

--- src/views/Invoices.vue
+++ src/views/Invoices.vue
@@ -3,3 +3,17 @@
     <h1>Invoices</h1>
   </div>
 </template>
+
+<script>
+import { onBeforeRouteLeave } from 'vue-router';
+
+export default {
+  name: 'Invoices',
+  setup() {
+    onBeforeRouteLeave((to, from, next) => {
+      const answer = window.confirm('Do you really want to leave?')
+      return answer ? next() : next(false)
+    })
+  }
+}
+</script>
