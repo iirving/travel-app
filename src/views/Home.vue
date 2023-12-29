926689952708c7f80e680ae7219ec118c8a4bf1e
Ian Irving <ian@falsepositives.com>
2023-12-29 17:28:00 -0500

    Add isLoggedIn computed property and triggerRouterError method to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,19 +12,39 @@
         </h2>
       </router-link>
     </div>
-
+    <div v-if="isLoggedIn">
+      <button @click="triggerRouterError">Trigger & Log Router Error</button>
+    </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import { isNavigationFailure, NavigationFailureType } from "vue-router"
 
 export default {
   data() {
     return {
-      destinations: sourceData.destinations
+      destinations: sourceData.destinations,
+      username: window.user
     }
+  },
+  computed: {
+    isLoggedIn() {
+      return !!this.username
+    }
+  },
+  methods: {
+    async triggerRouterError() {
+      const navigationFailure = await this.$router.push({ name: 'Home' })
+      if (isNavigationFailure(navigationFailure, NavigationFailureType.duplicated)) {
+        console.log('navigationFailure to', navigationFailure.to)
+        console.log('navigationFailure from', navigationFailure.from)
+      } else { //all is well hopefully
+      }
 
+
+    }
   }
 
 }
